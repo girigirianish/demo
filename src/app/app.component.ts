a8efbccc61bf5ed006232c387a921a97c27c6ca5
rmanandhar@sevadev.com <rmanandhar@sevadev.com>
2021-03-16 09:21:07 +0545

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash-todolist';
+  title:string = 'Todo List';
+
+  constructor(){
+    console.log(1+1)
+  }
 }
